9eefbd9478ddac789e64d272352ebcb0590ced76
Arkadiusz <asmilewski@gmail.com>
2017-10-18 20:37:21 +0200

    webpack dev-server set up

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: (path.join(__dirname, 'public')),
+        path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
     },
     module: {
@@ -13,5 +13,9 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_modules/
         }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
     }
 };
