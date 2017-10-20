6c45bb747e97f84b760cbf8a7355e204e55fc1fb
Arkadiusz <asmilewski@gmail.com>
2017-10-20 14:32:09 +0200

    Styling widget

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -6,91 +6,93 @@ import Options from './Options';
 import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
-    state = {
-        options: [],
-        selectedOption: undefined
-    };
-   
-    handleDeleteOptions = () => {
-        this.setState(() => ({options: []}))
-    }
-    
-    handleDeleteOption = (optionToRemove) => {
-        this.setState((prevState) => ({
-            options: prevState.options.filter((option) => {
-                return optionToRemove !== option;    
-            })
-        }));
-    }
+  state = {
+      options: [],
+      selectedOption: undefined
+  };
+  
+  handleDeleteOptions = () => {
+      this.setState(() => ({options: []}))
+  }
+  
+  handleDeleteOption = (optionToRemove) => {
+      this.setState((prevState) => ({
+          options: prevState.options.filter((option) => {
+              return optionToRemove !== option;    
+          })
+      }));
+  }
 
-    handlePick = () => {
-        const randomNum = Math.floor(Math.random() * this.state.options.length);
-        const option = this.state.options[randomNum]; 
-        this.setState(() => ({selectedOption: option}));
-    }
+  handlePick = () => {
+      const randomNum = Math.floor(Math.random() * this.state.options.length);
+      const option = this.state.options[randomNum]; 
+      this.setState(() => ({selectedOption: option}));
+  }
 
-    handleClearSelectedOption = () => {
-        this.setState(() => ({selectedOption: undefined}));
-    }
+  handleClearSelectedOption = () => {
+      this.setState(() => ({selectedOption: undefined}));
+  }
 
-    handleAddOption = (option) => {
-        if(!option) {
-          return 'Enter valid value to add item';
-        } else if(this.state.options.indexOf(option) > -1) {
-          return 'This option already exists';
-        } 
-        this.setState((prevState) => ({options: prevState.options.concat(option)}))
-    }
-    
-    componentDidMount() {
-        try {
-            const json = localStorage.getItem('options'); 
-            const options = JSON.parse(json);
-    
-            if(options) {
-                this.setState(() => ( {options: options })); 
-            }
-        }catch (e) {
-            
-        } 
-    }
+  handleAddOption = (option) => {
+      if(!option) {
+        return 'Enter valid value to add item';
+      } else if(this.state.options.indexOf(option) > -1) {
+        return 'This option already exists';
+      } 
+      this.setState((prevState) => ({options: prevState.options.concat(option)}))
+  }
+  
+  componentDidMount() {
+      try {
+          const json = localStorage.getItem('options'); 
+          const options = JSON.parse(json);
+  
+          if(options) {
+              this.setState(() => ( {options: options })); 
+          }
+      }catch (e) {
+          
+      } 
+  }
 
-    componentDidUpdate(prevProps, prevState) {
-        if(prevState.options.length !== this.state.options.length){
-            const json = JSON.stringify(this.state.options);
-            localStorage.setItem('options', json)
-            console.log('component did update');
-        }
-    }
-    componentWillUnmount() {
-        console.log('componentWillUnmonut')
-    }
-    
-    render () {
-       const subtitle = 'Put your life in the hands of computer';
-        return  (
-            <div>
-            <Header subtitle={subtitle} />
-                <div className='container'>
-                    <Action 
-                        hasOptions={this.state.options.length > 0}
-                        handlePick={this.handlePick}
-                    />
+  componentDidUpdate(prevProps, prevState) {
+      if(prevState.options.length !== this.state.options.length){
+          const json = JSON.stringify(this.state.options);
+          localStorage.setItem('options', json)
+          console.log('component did update');
+      }
+  }
+  componentWillUnmount() {
+      console.log('componentWillUnmonut')
+  }
+  
+  render () {
+      const subtitle = 'Put your life in the hands of computer';
+      return  (
+          <div>
+          <Header subtitle={subtitle} />
+              <div className='container'>
+                  <Action 
+                      hasOptions={this.state.options.length > 0}
+                      handlePick={this.handlePick}
+                  />
+                  <div className='widget'>
                     <Options
-                        options ={this.state.options}
-                        handleDeleteOptions={this.handleDeleteOptions}
-                        handleDeleteOption={this.handleDeleteOption}
+                      options ={this.state.options}
+                      handleDeleteOptions={this.handleDeleteOptions}
+                      handleDeleteOption={this.handleDeleteOption}
                     />
                     <AddOption 
-                        handleAddOption={this.handleAddOption}
+                      handleAddOption={this.handleAddOption}
                     />
-                </div>
-            
-            <OptionModal 
-             selectedOption={this.state.selectedOption}
-             handleClearSelectedOption={this.handleClearSelectedOption}
-            />
-            </div>
-        )
-    }
+                  </div>
+              </div>
+          
+          <OptionModal 
+            selectedOption={this.state.selectedOption}
+            handleClearSelectedOption={this.handleClearSelectedOption}
+          />
+          </div>
+      )
+  }
 };
