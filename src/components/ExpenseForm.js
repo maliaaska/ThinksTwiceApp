bd1ef597a971fb9331e8518b244969337237ed41
Arkadiusz <asmilewski@gmail.com>
2017-11-04 13:21:18 +0100

    addExpense Wired up

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -14,7 +14,8 @@ export default class ExpenseForm extends React.Component {
     note: "",
     amount:"",
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: '',
   };
   onDescriptionChange = (e) => {
     const description = e.target.value
@@ -26,22 +27,39 @@ export default class ExpenseForm extends React.Component {
   }
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if( amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if(!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
 
       this.setState(() => ({ amount }))
     }
   };
   onDateChange = (createdAt) => {
-    this.setState(() => ({ createdAt }));
-
+    if(createdAt){
+      this.setState(() => ({ createdAt }));
+    }
   }
   onFocusChange = ({ focused }) => {
     this.setState(()  => ({calendarFocused: focused}));
-  }
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    if(!this.state.description || !this.state.amount) {
+      this.setState(() => ({ error: 'Please provide Description and amount' }));
+    } else {
+      this.setState(() => ({error: '' }));
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
+      
+    }
+  };
   render() {
     return (
       <div>
-        <form action="">
+        {this.state.error && <h3>{this.state.error}</h3>}
+        <form onSubmit={this.onSubmit}>
           <input
             type='text'
             placeholder="Description"
@@ -68,7 +86,7 @@ export default class ExpenseForm extends React.Component {
             value={this.state.note}
             onChange={this.onNoteChange}
           />
-          <button>
+          <button onClick={this.onSubmit}>
             Add Expense
           </button>
         </form>
