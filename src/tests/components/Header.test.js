26b9bee873a3a9e524dfe5de066097316b7d6a18
Arkadiusz <asmilewski@gmail.com>
2017-11-23 18:17:50 +0100

    setting up enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow' 
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json'
 import Header from '../../components/Header';
 
 //react-test-render
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer;
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(toJSON(wrapper)).toMatchSnapshot();
+
+
+  // expect(wrapper.find('h1').text()).toBe("ThinkTwice");
+  // const renderer = new ReactShallowRenderer;
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();u
 });
