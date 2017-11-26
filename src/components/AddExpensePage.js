cb1119c754cd9b6b5b27336299f40a4ed325715f
Arkadiusz <asmilewski@gmail.com>
2017-11-26 21:11:45 +0100

    Testing AddExpensePage done

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm'
-import { addExpense } from '../actions/expenses'
-const AddExpensePage = (props) => (
-  <div>
-  <h1>Add Expense</h1>
-  <ExpenseForm 
-    onSubmit={(expense) => {
-      props.dispatch(addExpense(expense));
-      props.history.push('/');
-    }}
-  />
-  </div>
-);
+import { addExpense } from '../actions/expenses';
 
-export default connect()(AddExpensePage);
+export class AddExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    //props.dispatch(addExpense(expense));
+    this.props.onSubmit(expense);
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+      <h1>Add Expense</h1>
+      <ExpenseForm 
+        onSubmit={this.onSubmit}
+      />
+      </div>
+    );
+  };
+};
+
+
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit: (expense) => dispatch(addExpense(expense))
+});
+export default connect(undefined, mapDispatchToProps )(AddExpensePage);
