73b77b3beeb25eaed49ba132615af2c2839ca21b
Arkadiusz <asmilewski@gmail.com>
2017-11-27 18:45:20 +0100

    Production webpack done

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 
 const path = require('path');
 
-module.exports = {
+
+module.exports = (env) => {
+  const isProduction = env === 'production';
+ 
+  return {
     entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
@@ -21,9 +25,12 @@ module.exports = {
             ]
         }]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true
     }
-};
+  };
+  
+}
+
