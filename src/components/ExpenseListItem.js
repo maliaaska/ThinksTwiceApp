98a93a29e099a73ce3ee2892376e2dc8cf89053a
Arkadiusz <asmilewski@gmail.com>
2017-12-02 13:15:25 +0100

    setting formatting for amoiunt and createAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = ({dispatch, description, amount, createdAt, id}) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>   
-    <p>{amount} - {createdAt}</p>
+    <p>
+      {numeral(amount / 100).format('$0,0.00')} 
+      - 
+      {moment(createdAt).format('Do MMMM, YYYY')}
+    </p>
     <button value={id}
             onClick={() => {dispatch(removeExpense({ id }))}}
     >Remove</button>
--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -3,7 +3,6 @@ import { shallow } from 'enzyme';
 import { ExpenseListItem } from '../../components/ExpenseListItem';
 import expenses from '../fixtures/expenses';
 
-
 test('Should render ExpenseListItem  correctly', () => {
   const wrapper = shallow(<ExpenseListItem {...expenses[0]} />);
   expect(wrapper).toMatchSnapshot();
