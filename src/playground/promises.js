67bb5a8f57507ee5a58fc8cfbb42e4c93565cbf0
Arkadiusz <asmilewski@gmail.com>
2017-12-03 15:03:41 +0100

    ES6 promises recap

---
+++ src/playground/promises.js
@@ -0,0 +1,19 @@
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve({
+    //   name: "arek",
+    //   age: 38
+    // });
+    reject('Something went wrong!');
+  }, 5000)
+  
+});
+console.log('before');
+
+promise.then((data) => {
+  console.log(data);
+}).catch((error) => {
+  console.log('error: ', error);
+});
+
+console.log('after');
