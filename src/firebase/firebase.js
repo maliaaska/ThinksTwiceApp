7b395154b1d5888bbbd5f1afa7908acd54223aef
Arkadiusz <asmilewski@gmail.com>
2017-12-23 20:13:47 +0100

    setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyACXaQmFZvA5Yw2RzNwSfRpar-EHJ-cbKk",
-  authDomain: "thinktwice-bcb0d.firebaseapp.com",
-  databaseURL: "https://thinktwice-bcb0d.firebaseio.com",
-  projectId: "thinktwice-bcb0d",
-  storageBucket: "thinktwice-bcb0d.appspot.com",
-  messagingSenderId: "463345727650"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
