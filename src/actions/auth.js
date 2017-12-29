1300cc77e627daa85f803d440eef2868fba7c136
Arkadiusz <asmilewski@gmail.com>
2017-12-29 13:08:04 +0100

    add private routes

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,10 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase';
 
+export const login = (uid) => ({ 
+  type: 'LOGIN',
+  uid
+});
+
 
 export const startLogin = () => {
   return () => {
@@ -8,6 +13,9 @@ export const startLogin = () => {
   };
 };
 
+export const logout = () => ({
+  type: "LOGOUT"
+});
 
 export const startLogout = () => {
   return () => {
